95bf3dc96a9baa38114078ba148fad7b8b0fcbe9
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-08 18:37:43 +0200

    add homework

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
+
+  clonedBtn.innerText = "Я изменю мир";
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
+
+    // className - присвоение класса
+  // clonedBtn.className = "second-btn";
+  // задать аттрибуту (какому) и (какое)
+  clonedBtn.setAttribute("class", "second-btn") 
+
+  console.log(clonedBtn);
+  document.body.appendChild(clonedBtn);
 })
 
 // здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click', ()=>{
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";
+})
